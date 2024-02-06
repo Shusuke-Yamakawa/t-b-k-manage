b243812bc50b79acb424629090127a61462eb751
biz-yamakawa <105332545+biz-yamakawa@users.noreply.github.com>
2024-02-06 09:49:21 +0900

    ゲスト登録でライン通知されるように実装

--- src/app/_lib/db/guest.ts
+++ src/app/_lib/db/guest.ts
@@ -3,6 +3,7 @@ import { prisma } from '@/src/app/_lib/prisma';
 export type Guest = {
   guest_nm: string;
   court_id: number;
+  invited_card_id: string;
 };
 
 export const createGuest = async (params: Guest) => prisma.guest.create({ data: params });
--- src/app/entry/[id]/page.tsx
+++ src/app/entry/[id]/page.tsx
@@ -7,14 +7,29 @@ import { findGetCourtById } from '@/src/app/_lib/db/getCourt';
 import { EntryDetail } from '@/src/app/entry/_components/EntryDetail';
 import { EntryDataWithCardAll } from '@/src/app/court/_types/court.type';
 import { createGuest } from '@/src/app/_lib/db/guest';
+import { notify_line } from '@/src/app/_utils/line';
 
 export const dynamic = 'force-dynamic';
 
 const guestAdd = async (formData: { guestName: string; courtId: number }) => {
   'use server';
 
+  const session = await getServerSession(authOptions);
+
   const { guestName, courtId } = formData;
-  await createGuest({ guest_nm: guestName, court_id: courtId });
+  await createGuest({
+    guest_nm: guestName,
+    court_id: courtId,
+    invited_card_id: session.user.card_id,
+  });
+  const getCourt = await findGetCourtById(courtId);
+  const msg = `${getCourt!.month}/${getCourt!.day} ${getCourt!.from_time}-${
+    getCourt!.to_time
+  }@${getCourt!.court.slice(0, -2)}
+
+ゲスト名：${guestName}
+招待者：${session.user.nick_nm}`;
+  notify_line(msg, 'AmiDotEhZrkZWaGSc6a5HQNwtUbERzCcGPEuQ7tEwBG');
   revalidatePath('/entry/[id]', 'page');
 };
 
