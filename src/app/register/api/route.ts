cdfa30adffabfdeea4be2fc3412ff01eb8ac13a9
Shusuke-Yamakawa <shusuke.y@aol.jp>
2024-03-01 13:57:09 +0900

    カード登録フォームを作成

---
+++ src/app/register/api/route.ts
@@ -0,0 +1,28 @@
+import { createCard } from '@/src/app/_lib/db/card';
+import { notify_line } from '@/src/app/_utils/line';
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(req: Request) {
+  const { cardId, password, userNm, nickNm } = await req.json();
+  try {
+    await createCard({
+      card_id: cardId,
+      password,
+      user_nm: userNm,
+      nick_nm: nickNm,
+      available_flg: true,
+      draw_flg: true,
+      admin_flg: false,
+      note: '新システム',
+    });
+    const msg = nickNm || userNm;
+    notify_line(msg, '7DAsvr7QdC0sDF6wW4HGg6aDcwJmfbiE98J7zbE8V4B');
+  } catch (e) {
+    console.log('登録エラー ', e);
+  }
+
+  return new Response(JSON.stringify({ message: '' }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -8,71 +8,79 @@ import {
   Container,
   Button,
   LoadingOverlay,
-  Text,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { zodResolver } from 'mantine-form-zod-resolver';
 import { z } from 'zod';
-import { signIn } from 'next-auth/react';
 import { useDisclosure } from '@mantine/hooks';
-import Link from 'next/link';
+import axios from 'axios';
+import { API_URL } from '@/src/app/_consts/environment.const';
 
 const schema = z.object({
   cardId: z.string().min(8, { message: 'IDは8文字です' }).max(8, { message: 'IDは8文字です' }),
-  password: z.string().trim(),
-  // .min(10, { message: 'パスワードは8文字です' })
-  // .max(24, { message: 'パスワードは8文字です' }),
+  password: z
+    .string()
+    .min(10, { message: 'パスワードは10文字以上入れて' })
+    .max(24, { message: 'パスワードは24文字以内' })
+    .trim(),
+  userNm: z.string().min(1, { message: '名前は必須です' }).trim(),
+  nickNm: z.string().trim(),
 });
 
-export default function LoginPage() {
+export default function ResisterPage() {
   const form = useForm({
     initialValues: {
       cardId: '',
       password: '',
+      userNm: '',
+      nickNm: '',
     },
     validate: zodResolver(schema),
   });
 
-  const [visible, { open }] = useDisclosure(false);
+  const [visible, { open, close }] = useDisclosure(false);
 
   return (
     <Container size={420} my={40}>
-      <Title ta="center">お主T.B.K.か</Title>
+      <Title ta="center">T.B.K.にようこそ</Title>
       <LoadingOverlay visible={visible} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
       <Paper withBorder shadow="md" p={30} mt={30} radius="md">
         <form
           onSubmit={form.onSubmit(async (values) => {
+            console.log('values: ', values);
             open();
-            const result = await signIn('user', { redirect: false, ...values });
-            if (result?.error) {
-              form.setFieldError('cardId', 'IDかパスワードが誤っています');
-              form.setFieldError('password', 'IDかパスワードが誤っています');
-              open();
-            } else {
-              // ログイン成功時トップページへリダイレクト
-              window.location.href = '/court';
-            }
+            await axios.post(`${API_URL}register/api/`, {
+              ...values,
+            });
+            close();
           })}
         >
           <TextInput
             label="CardId"
-            placeholder="10007001"
+            placeholder="80097001"
             maxLength={8}
             {...form.getInputProps('cardId')}
           />
           <PasswordInput
             label="Password"
-            placeholder="都営システムのパスワード"
+            placeholder="生年月日を入れてください"
             maxLength={24}
             mt="md"
             {...form.getInputProps('password')}
           />
+          <TextInput
+            label="ユーザー名"
+            placeholder="ドイ ケイコ"
+            {...form.getInputProps('userNm')}
+          />
+          <TextInput
+            label="ニックネーム"
+            placeholder="任意入力です"
+            {...form.getInputProps('nickNm')}
+          />
           <Button type="submit" fullWidth mt="xl">
-            Sign in
+            新規登録
           </Button>
-          <Text ta="center" mt="md">
-            新規登録の方は <Link href="/register">こちら</Link>
-          </Text>
         </form>
       </Paper>
     </Container>
