e63ba97b0d28777e641bd065753d37bfadfefc1a
Shusuke-Yamakawa <shusuke.y@aol.jp>
2025-01-27 19:43:40 +0900

    notify消す

--- src/app/_utils/line.ts
+++ src/app/_utils/line.ts
@@ -1,26 +1,4 @@
 import axios from "axios";
-import qs from "qs";
-
-export const notify_line = async (
-  msg: string,
-  token = "0qWBwrgX4NNRwvW437ts2PMwJqTccdke934hBetGAqh",
-) => {
-  const BASE_URL = "https://notify-api.line.me";
-  const PATH = "/api/notify";
-  const config = {
-    baseURL: BASE_URL,
-    url: PATH,
-    method: "post",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: `Bearer ${token}`,
-    },
-    data: qs.stringify({
-      message: `\n${msg}`,
-    }),
-  };
-  await axios.request(config);
-};
 
 export const notifyLineMessage = async (msg: string) => {
   const token =
